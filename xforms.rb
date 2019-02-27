e35e52fa561bcc2700f5f3396e23fa067d101173
Kazuhiro Sakai <Kazuhiro.Sakai@nasa.gov>
2019-02-27 13:58:22 -0500

    Added xforms

---
+++ xforms.rb
@@ -0,0 +1,17 @@
+class Xforms < Formula
+  desc "XForms Toolkit"
+  homepage "http://xforms-toolkit.org"
+  url "http://download.savannah.nongnu.org/releases/xforms/xforms-1.2.4.tar.gz"
+  sha256 "78cc6b07071bbeaa1f906e0a22d5e9980e48f8913577bc082d661afe5cb75696"
+  # depends_on "cmake" => :build
+  
+  depends_on :x11
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
